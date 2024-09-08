7cd04d674a0291f04043a097c1d5251509228c30
iksergey <i@ksergey.ru>
2024-09-08 14:48:37 +0300

    Описание AppUserService

---
+++ src/main/java/ru/ksergey/ContactsApp/service/jwt/AppUserService.java
@@ -0,0 +1,32 @@
+package ru.ksergey.ContactsApp.service.jwt;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ru.ksergey.ContactsApp.model.AppUser;
+import ru.ksergey.ContactsApp.repository.AppUserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserService {
+
+    private AppUserRepository appUserRepository;
+
+    public UserDetailsService getDetailsService() {
+
+        UserDetailsService detailsService = new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser user = appUserRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+                return user;
+            }
+        };
+
+        return detailsService;
+    }
+
+}
