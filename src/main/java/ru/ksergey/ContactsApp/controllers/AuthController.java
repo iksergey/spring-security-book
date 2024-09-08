f1ecd0798969e5b9290996cbbb39d22b3dac918e
iksergey <i@ksergey.ru>
2024-09-08 18:44:25 +0300

    Endpoint логина

--- src/main/java/ru/ksergey/ContactsApp/controllers/AuthController.java
+++ src/main/java/ru/ksergey/ContactsApp/controllers/AuthController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import ru.ksergey.ContactsApp.model.AppUser;
+import ru.ksergey.ContactsApp.modelDto.LoginRequestDto;
+import ru.ksergey.ContactsApp.modelDto.LoginResponseDto;
 import ru.ksergey.ContactsApp.modelDto.RegisterRequestDto;
 import ru.ksergey.ContactsApp.service.AuthService;
 
@@ -21,6 +23,11 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<AppUser> register(@RequestBody RegisterRequestDto registerRequestDto) {
         return ResponseEntity.ok(authService.register(registerRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(authService.login(loginRequestDto));
     }
 
 }
