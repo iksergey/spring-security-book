d5195b88b1c0b5ad17c5f82b641574d3e0001e4d
iksergey <i@ksergey.ru>
2024-09-08 14:49:36 +0300

    Описание JwtAuthenticationFilter

---
+++ src/main/java/ru/ksergey/ContactsApp/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package ru.ksergey.ContactsApp.configuration;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import ru.ksergey.ContactsApp.service.jwt.AppUserService;
+import ru.ksergey.ContactsApp.service.jwt.JwtSecurityService;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtSecurityService jwtSecurityService;
+    private AppUserService appUserService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (StringUtils.isEmpty(header)
+                || !org.apache.commons.lang3.StringUtils.startsWith(header, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = header.substring(7);
+        String email = jwtSecurityService.extractUsername(jwt);
+
+        if (StringUtils.isNotEmpty(email)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = appUserService
+                    .getDetailsService()
+                    .loadUserByUsername(email);
+
+            if (jwtSecurityService.validateToken(jwt, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
