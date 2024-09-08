24d42f0fe38384182296977df53aea43ef70bf08
iksergey <i@ksergey.ru>
2024-09-08 18:39:07 +0300

    Логика получения jwt-токена

--- src/main/java/ru/ksergey/ContactsApp/service/AuthService.java
+++ src/main/java/ru/ksergey/ContactsApp/service/AuthService.java
@@ -1,19 +1,30 @@
 package ru.ksergey.ContactsApp.service;
 
+import java.util.HashMap;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import ru.ksergey.ContactsApp.enums.AppRole;
 import ru.ksergey.ContactsApp.model.AppUser;
+import ru.ksergey.ContactsApp.modelDto.LoginRequestDto;
+import ru.ksergey.ContactsApp.modelDto.LoginResponseDto;
 import ru.ksergey.ContactsApp.modelDto.RegisterRequestDto;
 import ru.ksergey.ContactsApp.repository.AppUserRepository;
+import ru.ksergey.ContactsApp.service.jwt.JwtSecurityService;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final AppUserRepository appUserRepository;
     private final PasswordEncoder passwordEncoder;
+
+    private final JwtSecurityService jwtSecurityService;
+    private final AuthenticationManager authenticationManager;
 
     public AppUser register(RegisterRequestDto registerRequestDto) {
         AppUser appUser = new AppUser();
@@ -25,4 +36,24 @@ public class AuthService {
         return appUserRepository.save(appUser);
     }
 
+    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequestDto.getEmail(),
+                        loginRequestDto.getPassword()));
+
+        AppUser user = appUserRepository
+                .findByEmail(loginRequestDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Ошибка"));
+
+        String token = jwtSecurityService.generateToken(user);
+        String refreshToken = jwtSecurityService.generateRefreshToken(new HashMap<>(), user);
+
+        return LoginResponseDto
+                .builder()
+                .email(loginRequestDto.getEmail())
+                .jwtToken(token)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
