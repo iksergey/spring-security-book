9677c1350d28e11c66ec12abe015f11418d3688f
iksergey <i@ksergey.ru>
2024-09-08 14:47:53 +0300

    Обновление AppUserRepository

--- src/main/java/ru/ksergey/ContactsApp/repository/AppUserRepository.java
+++ src/main/java/ru/ksergey/ContactsApp/repository/AppUserRepository.java
@@ -1,5 +1,7 @@
 package ru.ksergey.ContactsApp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import ru.ksergey.ContactsApp.model.AppUser;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-
+    Optional<AppUser> findByEmail(String email);
 }
