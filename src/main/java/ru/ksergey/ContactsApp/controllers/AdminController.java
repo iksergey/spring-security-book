e28c849d2cc9d1a70f5fd14c2b7923082b421d63
iksergey <i@ksergey.ru>
2024-09-08 19:40:27 +0300

    Тест запросов с разными правами

---
+++ src/main/java/ru/ksergey/ContactsApp/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package ru.ksergey.ContactsApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Ok");
+    }
+}
